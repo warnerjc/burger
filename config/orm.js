7d79d13f19192e25bef31ea9f423b33e3fb9a7d5
Jason Warner <jcwarner@gmail.com>
2018-05-08 19:27:49 -0500

    Initial config files for Node and MySQL db connection, plus ORM

---
+++ config/orm.js
@@ -0,0 +1,24 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+// ORM object with methods that will execute
+// MySQL commands in the controllers
+let orm = {
+
+    selectAll: function() {
+
+    },
+
+    insertOne: function() {
+
+    },
+
+    updateOne: function() {
+
+    }
+    
+};
+
+// Export the orm object for the model.
+module.exports = orm;
+
