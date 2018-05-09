ce411ee7ef2d7b6216d62056c208bdcf24effe5f
Jason Warner <jcwarner@gmail.com>
2018-05-08 19:41:42 -0500

    Initial Controller added for the application

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model to use its database functions.
+const burger = require("../models/burgers.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.create(["burger_name"], [req.body.burgerName], function (result) {
+        // Send back the ID of the new burger
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function (result) {
+            if (result.changedRows === 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            }
+            res.status(200).end();
+
+        }
+    );
+});
+
+// Export routes for server.js to use.
+module.exports = router;
