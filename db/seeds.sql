698d03877b7f68aaa05606960affb16e43c926c2
Jason Warner <jcwarner@gmail.com>
2018-05-08 19:10:36 -0500

    Initial SQL Database Schema and Seed Files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Plain Jane Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Juicy Jupiter Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Down Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Chili Cheeseburger');
