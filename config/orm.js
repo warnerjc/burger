559f2dc7c91addcbe89eefcf036a395fe8c58239
Jason Warner <jcwarner@gmail.com>
2018-05-08 19:59:25 -0500

    Initial handlebar views added to application

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,5 @@
 // Import MySQL connection.
-var connection = require("../config/connection.js");
+const connection = require("../config/connection.js");
 
 // ORM object with methods that will execute
 // MySQL commands in the controllers
